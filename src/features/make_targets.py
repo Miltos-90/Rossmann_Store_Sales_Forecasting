1b5799d6f064b2556b74393c0e2cbf3136dace26
Miltos-90 <35740991+Miltos-90@users.noreply.github.com>
2026-06-07 18:39:17 +0200

    sorting

--- src/features/make_targets.py
+++ src/features/make_targets.py
@@ -13,7 +13,8 @@ def make_targets(df: pd.Series, horizon: int) -> pd.DataFrame:
         A DataFrame with the same MultiIndex and new columns for each future target, named as '-1', '-2', ..., '-horizon'.
     """
 
-    lags = range(-1, -(horizon + 1), -1)
+    #lags = range(-1, -(horizon + 1), -1)
+    lags = [-horizon]
     lag_df = make_lags(df.reset_index() , lags, names=[f'{-l}' for l in lags])
     lag_df.set_index(['Date', 'Store'], inplace=True)
     return lag_df
