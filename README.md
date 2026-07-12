938b16eb8744e1bbf3f0095fe755573efebe8e98
Miltos-90 <35740991+Miltos-90@users.noreply.github.com>
2026-07-12 12:39:42 +0200

    infra

--- README.md
+++ README.md
@@ -1,15 +1,1 @@
 # Rossmann Store Sales Forecasting
-
-## Provision
-- Azure Resource Group
-- Azure Storage account (Blob container)
-  - Use BlobFuse2 to mount the blob container as a local directory on the VM
-- Spot VM
-  - [VM Size](https://azure.microsoft.com/en-us/pricing/spot-advisor)
-    - E16ps v5 -> 16 vCPUs, 128 GB RAM -> Spot instance cost 0.17 USD/hr
-  - Azure Spot Discount: Set to Enable
-  - Eviction Policy: Set to Delete
-- Managed ID asigned to the VM so it can read/write to the storage account
-
-
-I am trying to run a single Standard_E16ps_v5 Spot VM for a workload, which requires 16 cores.
--- infra/main.tf
+++ infra/main.tf
@@ -1,25 +1,34 @@
+######### CONSTANTS #########
+
 locals {
   resource_group_location = "eastus"
-
   storage_account = {
     tier             = "Standard"
     replication_type = "LRS" # Cheapest option
   }
 
-  virtual_machine = {
-    instance_type   = "Standard_e17_v5" # 16 vCPUs, 128 GiB RAM, No storage
+  use_spot_vm = true # true: Spot VM, false: Regular VM
+
+  virtual_machine = local.use_spot_vm ? {
+    # Spot VM configuration (pricing: https://azure.microsoft.com/en-us/pricing/spot-advisor/#pricing)
+    instance_type   = "Standard_E16as_v7" # E16as v7: 16 vCPUs, 128 GiB RAM, No storage
     priority        = "Spot"
-    eviction_policy = "Delete" # Deletes the OS disk upon eviction to stop billing entirely
-    max_bid_price   = -1       # Default to capacity-only eviction
-    admin_username  = "azureuser"
-    ssh_public_key  = "C:\\Users\\m_kal\\.ssh\\id_rsa.pub"
+    eviction_policy = "Delete"
+    max_bid_price   = -1
+  } : {
+    # Regular VM configuration (pricing: https://azure.microsoft.com/en-us/pricing/details/virtual-machines/linux/#pricing)
+    instance_type   = "Standard_B2ls_v2" # B2ls v2: 2 vCPUs, 4 GiB RAM, No storage
+    priority        = "Regular"
+  }
+
+  # VM constants
+  vm_ssh_key_path = "C:\\Users\\m_kal\\.ssh\\id_rsa.pub"
     # Run command `ssh-keygen -t rsa -b 4096` to generate a new SSH key pair if you don't have one already. 
     # Press Enter to accept the default file location. Press Enter again to skip setting a passphrase. 
     # Write down the location of the private key (usually C:\Users\<username>\.ssh\id_rsa) and keep it safe.
-  }
 }
 
-
+######### CONFIGURATION #########
 terraform {
   required_version = ">= 1.0.0"
 
@@ -39,6 +48,7 @@ provider "azurerm" {
   }
 }
 
+# Resource Group
 resource "azurerm_resource_group" "ml_rg" {
   name     = "rg-ml-spot-minimal"
   location = local.resource_group_location
@@ -62,7 +72,7 @@ resource "azurerm_storage_container" "artifacts" {
 
 # RBAC
 resource "azurerm_user_assigned_identity" "vm_identity" {
-  name                = "id-spot-vm-executor"
+  name                = "id-vm-executor"
   resource_group_name = azurerm_resource_group.ml_rg.name
   location            = local.resource_group_location
 }
@@ -76,21 +86,21 @@ resource "azurerm_role_assignment" "storage_contrib" {
 
 # Networking
 resource "azurerm_virtual_network" "ml_vnet" {
-  name                = "vnet-ml-minimal"
+  name                = "vnet-ml"
   address_space       = ["10.0.0.0/16"]
   location            = local.resource_group_location
   resource_group_name = azurerm_resource_group.ml_rg.name
 }
 
 resource "azurerm_subnet" "ml_subnet" {
-  name                 = "snet-minimal"
+  name                 = "snet-vm"
   resource_group_name  = azurerm_resource_group.ml_rg.name
   virtual_network_name = azurerm_virtual_network.ml_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_interface" "ml_nic" {
-  name                = "nic-ml-vm"
+  name                = "nic-vm"
   location            = local.resource_group_location
   resource_group_name = azurerm_resource_group.ml_rg.name
 
@@ -103,19 +113,18 @@ resource "azurerm_network_interface" "ml_nic" {
 
 # Virtual Machine
 resource "azurerm_linux_virtual_machine" "spot_vm" {
-  name                = "vm-ml-spot-worker"
+  name                = "vm-worker"
+  admin_username      = "azureuser"
   resource_group_name = azurerm_resource_group.ml_rg.name
   location            = local.resource_group_location
   size                = local.virtual_machine.instance_type
-  admin_username      = local.virtual_machine.admin_username
-
-  priority        = local.virtual_machine.priority
-  eviction_policy = local.virtual_machine.eviction_policy # Deletes the OS disk upon eviction to stop billing entirely
-  max_bid_price   = local.virtual_machine.max_bid_price   # Default to capacity-only eviction
+  priority            = local.virtual_machine.priority
+  eviction_policy     = local.virtual_machine.eviction_policy # Deletes the OS disk upon eviction to stop billing entirely
+  max_bid_price       = local.virtual_machine.max_bid_price   # Default to capacity-only eviction
 
   admin_ssh_key {
-    username   = local.virtual_machine.admin_username
-    public_key = file(local.virtual_machine.ssh_public_key)
+    username   = "azureuser"
+    public_key = file(local.vm_ssh_key_path)
   }
 
   network_interface_ids = [
